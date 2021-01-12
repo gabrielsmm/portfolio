ea1a58d79641ea05d995dd6e0ad62663cd8f3d7d
Gabriel Mendes <gabrielsmm251@gmail.com>
2021-01-12 09:13:49 -0300

    Add form e-mail, scroll suave

---
+++ js/scroll.js
@@ -0,0 +1,27 @@
+function scrollTo(element){
+    document.querySelector(element).scrollIntoView({ behavior : "smooth" })
+}
+
+document.querySelector("#linkConhecimentos").addEventListener("click", function(event){
+    event.preventDefault()
+
+    scrollTo("#conhecimentos")
+})
+
+document.querySelector("#linkProjetos").addEventListener("click", function (event) {
+    event.preventDefault()
+
+    scrollTo("#projetos")
+})
+
+document.querySelector("#linkContato").addEventListener("click", function (event) {
+    event.preventDefault()
+
+    scrollTo("#contato")
+})
+
+document.querySelector("#linkInicio").addEventListener("click", function (event) {
+    event.preventDefault()
+
+    scrollTo("#inicio")
+})
