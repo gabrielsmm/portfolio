71e47a3dc5ac1e412fce2239176d4c94f4ad7b68
gabrielsmm <gabrielsmm251@gmail.com>
2023-01-23 21:48:46 -0300

    Adição de animações e melhorias

--- js/scroll.js
+++ js/scroll.js
@@ -31,3 +31,21 @@ document.querySelector("#linkArrow").addEventListener("click", function (event) 
 
     scrollTo("#sobre")
 })
+
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 30,
+    showCursor: false
+});
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '20px',
+    duration: 1500,
+    reset: true
+});
+
+sr.reveal('.tecnologia', { delay: 150 });
+sr.reveal('.projeto', { delay: 150 });
+sr.reveal('.contato-box', { delay: 150 });
+sr.reveal('.mensagem-container', { delay: 150 });
